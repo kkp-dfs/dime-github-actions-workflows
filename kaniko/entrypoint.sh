29dd040fae3e933b37cf53bfe04473caeddbd88c
Parse Pattamabenjakun <parse.patt@kkpfg.com>
2022-02-14 11:27:21 +0700

    use arguments in kaniko entrypoint script

--- kaniko/entrypoint.sh
+++ kaniko/entrypoint.sh
@@ -2,9 +2,13 @@
 
 cp -R ~/.docker/config.json /kaniko/.docker/config.json
 
+DESTINATIONS=$(echo $1 | sed -e "s| | --destination=|g")
+LABELS=$(echo $2 | sed -e "s| | --label=|g")
+
 /kaniko/executor \
     --cache \
     --dockerfile=Dockerfile \
-    --destination=$1
+    --destination=$DESTINATIONS \
+    --label=$LABELS
 
 echo "::set-output name=image::$1"
