6566f4f3aa87a5b7f7a28ae781dc66665e368d90
Parse Pattamabenjakun <parse.patt@kkpfg.com>
2022-02-16 16:12:17 +0700

    add build-args inputs in kaniko action

--- kaniko/entrypoint.sh
+++ kaniko/entrypoint.sh
@@ -2,13 +2,14 @@
 
 cp -R ~/.docker/config.json /kaniko/.docker/config.json
 
-DESTINATIONS=$(echo $1 | sed -e "s| | --destination=|g")
-LABELS=$(echo $2 | sed -e "s| | --label=|g")
+DESTINATIONS=$(echo $1 | tr " " "\n" | xargs -I@ echo --destination=@)
+BUILD_ARGS=$(echo $2 | tr " " "\n" | xargs-I@ echo --build-arg=@)
+LABELS=$(echo $3 | tr " " "\n" | xargs -I@ echo --label=@)
 
 /kaniko/executor \
     --cache \
     --dockerfile=Dockerfile \
     --context=/github/workspace \
-    --destination=$DESTINATIONS
+    $BUILD_ARGS $LABELS $DESTINATIONS
 
 echo "::set-output name=image::$1"
