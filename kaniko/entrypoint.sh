1aec8e37dd1e7d2eb4b9c1da75c96f2544f102d8
Parse Pattamabenjakun <parse.patt@kkpfg.com>
2022-02-18 02:03:32 +0700

    fix kaniko argument

--- kaniko/entrypoint.sh
+++ kaniko/entrypoint.sh
@@ -3,7 +3,7 @@
 cp -R ~/.docker/config.json /kaniko/.docker/config.json
 
 DESTINATIONS=$(echo $1 | tr " " "\n" | xargs -I@ echo --destination=@)
-BUILD_ARGS=$(echo $2 | tr " " "\n" | xargs-I@ echo --build-arg=@)
+BUILD_ARGS=$(echo $2 | tr " " "\n" | xargs -I@ echo --build-arg=@)
 LABELS=$(echo $3 | tr " " "\n" | xargs -I@ echo --label=@)
 
 /kaniko/executor \
