04aba0f815b6ea9cde2a4010c0f803748fe6773a
Parse Pattamabenjakun <parse.patt@kkpfg.com>
2022-02-23 02:13:11 +0700

    set get ecr info outputs

--- get-ecr-info/entrypoint.sh
+++ get-ecr-info/entrypoint.sh
@@ -7,8 +7,7 @@ TOKEN=$(aws ecr get-authorization-token \
     --region ap-southeast-1 --output text \
     --query 'authorizationData[].authorizationToken' | base64 -d)
 
-echo "::add-mask::$TOKEN"
-echo "::set-output name=token:$TOKEN"
+echo "::set-output name=token::$TOKEN"
 echo "::set-output name=repository::$REPOSITORY"
 echo "::set-output name=username::$(echo $TOKEN | cut -d: -f1)"
 echo "::set-output name=password::$(echo $TOKEN | cut -d: -f2)"
