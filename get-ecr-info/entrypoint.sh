bac0a3fb6e4b19f358aa3d227f3b868d73aec9cb
Parse Pattamabenjakun <parse.patt@kkpfg.com>
2022-02-23 01:37:12 +0700

    fix build action's arguments

--- get-ecr-info/entrypoint.sh
+++ get-ecr-info/entrypoint.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
+REPOSITORY=$(aws ecr describe-repositories --region ap-southeast-1 --output text \
+--query 'repositories[].repositoryUri' --repository-name "$1")
+
 TOKEN=$(aws ecr get-authorization-token \
     --region ap-southeast-1 --output text \
     --query 'authorizationData[].authorizationToken' | base64 -d)
 
 echo "::add-mask::$TOKEN"
+echo "::set-output name=token:$TOKEN"
+echo "::set-output name=repository::$REPOSITORY"
 echo "::set-output name=username::$(echo $TOKEN | cut -d: -f1)"
 echo "::set-output name=password::$(echo $TOKEN | cut -d: -f2)"
