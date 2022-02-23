e18152359730423ec542d2d5cd2dc64078fc0236
Parse Pattamabenjakun <parse.patt@kkpfg.com>
2022-02-23 09:19:03 +0700

    fix ecr token and no cache golangci-lint

--- get-ecr-info/entrypoint.sh
+++ get-ecr-info/entrypoint.sh
@@ -5,9 +5,9 @@ REPOSITORY=$(aws ecr describe-repositories --region ap-southeast-1 --output text
 
 TOKEN=$(aws ecr get-authorization-token \
     --region ap-southeast-1 --output text \
-    --query 'authorizationData[].authorizationToken' | base64 -d)
+    --query 'authorizationData[].authorizationToken')
 
 echo "::set-output name=token::$TOKEN"
 echo "::set-output name=repository::$REPOSITORY"
-echo "::set-output name=username::$(echo $TOKEN | cut -d: -f1)"
-echo "::set-output name=password::$(echo $TOKEN | cut -d: -f2)"
+echo "::set-output name=username::$(echo $TOKEN | base64 -d | cut -d: -f1)"
+echo "::set-output name=password::$(echo $TOKEN | base64 -d | cut -d: -f2)"
