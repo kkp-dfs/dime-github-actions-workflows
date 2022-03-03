448c2baf7d141d604d19e89c7f29d185210f63e9
Parse Pattamabenjakun <parse.patt@kkpfg.com>
2022-03-04 05:45:01 +0700

    create docker config.json in kaniko action

--- kaniko/entrypoint.sh
+++ kaniko/entrypoint.sh
@@ -1,12 +1,18 @@
 #!/busybox/sh
 
+PATH=/usr/local/bin:/kaniko:/busybox
+
 set -e
 
-cp -R ~/.docker/config.json /kaniko/.docker/config.json
+mkdir -p ~/.docker
+cat > ~/.docker/config.json <<EOF
+{"auths":{"$1":{"auth":"$2"}}}
+EOF
+cp ~/.docker/config.json /kaniko/.docker/config.json
 
-DESTINATIONS=$(echo $1 | tr " " "\n" | xargs -I@ echo --destination=@)
-BUILD_ARGS=$(echo $2 | tr " " "\n" | xargs -I@ echo --build-arg=@)
-LABELS=$(echo $3 | tr " " "\n" | xargs -I@ echo --label=@)
+DESTINATIONS=$(echo $3 | tr " " "\n" | xargs -I@ echo --destination=@)
+BUILD_ARGS=$(echo $4 | tr " " "\n" | xargs -I@ echo --build-arg=@)
+LABELS=$(echo $5 | tr " " "\n" | xargs -I@ echo --label=@)
 
 /kaniko/executor \
     --cache \
@@ -14,4 +20,4 @@ LABELS=$(echo $3 | tr " " "\n" | xargs -I@ echo --label=@)
     --context=/github/workspace \
     $BUILD_ARGS $DESTINATIONS
 
-echo "::set-output name=image::$1"
+echo "::set-output name=image::$3"
