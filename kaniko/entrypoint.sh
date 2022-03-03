a7623043779de5bc6259711ded44e0f03ccb8acb
Parse Pattamabenjakun <parse.patt@kkpfg.com>
2022-03-03 18:39:31 +0700

    get kaniko exit code

--- kaniko/entrypoint.sh
+++ kaniko/entrypoint.sh
@@ -6,10 +6,12 @@ DESTINATIONS=$(echo $1 | tr " " "\n" | xargs -I@ echo --destination=@)
 BUILD_ARGS=$(echo $2 | tr " " "\n" | xargs -I@ echo --build-arg=@)
 LABELS=$(echo $3 | tr " " "\n" | xargs -I@ echo --label=@)
 
+echo "::set-output name=image::$1"
+
 /kaniko/executor \
     --cache \
     --dockerfile=Dockerfile \
     --context=/github/workspace \
     $BUILD_ARGS $DESTINATIONS
 
-echo "::set-output name=image::$1"
+echo $?
