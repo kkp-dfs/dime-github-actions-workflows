6a16b2c7037ed164af80605e91dcbcf3c2c67df9
Parse Pattamabenjakun <parse.patt@kkpfg.com>
2022-03-08 17:36:20 +0700

    fix commit length

--- retagging-image/entrypoint.sh
+++ retagging-image/entrypoint.sh
@@ -3,10 +3,10 @@
 set -e
 
 REPOSITORY=$1
-CURRENT=$2
+COMMIT=$(echo $2 | cut -c-7)
 TAG=$3
 
 yum install -y jq
 MANIFEST=$(aws ecr batch-get-image --repository-name $REPOSITORY --image-ids imageTag=$CURRENT --output json | jq --raw-output '.images[0].imageManifest')
-aws ecr batch-delete-image --repository-name $REPOSITORY --image-ids imageTag=$TAG | jq -r .
+aws ecr batch-delete-image --repository-name $REPOSITORY --image-ids imageTag=$COMMIT | jq -r .
 aws ecr put-image --repository-name $REPOSITORY --image-tag $TAG --image-manifest "$MANIFEST" | jq -r .
