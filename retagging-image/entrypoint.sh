ab0c86711f5465b0fb782ee6732bd61a9f8e16e6
Parse Pattamabenjakun <parse.patt@kkpfg.com>
2022-03-08 18:39:19 +0700

    return error when no found tag

--- retagging-image/entrypoint.sh
+++ retagging-image/entrypoint.sh
@@ -8,5 +8,9 @@ TAG=$3
 
 yum install -y jq
 MANIFEST=$(aws ecr batch-get-image --repository-name $REPOSITORY --image-ids imageTag=$COMMIT --output json | jq --raw-output '.images[0].imageManifest')
+if [ "$MANIFEST" = "null" ]; then
+    exit 1
+if
+
 aws ecr batch-delete-image --repository-name $REPOSITORY --image-ids imageTag=$COMMIT | jq -r .
 aws ecr put-image --repository-name $REPOSITORY --image-tag $TAG --image-manifest "$MANIFEST" | jq -r .
