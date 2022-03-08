1d46d0c005099643fa165872900c45e363955238
Parse Pattamabenjakun <parse.patt@kkpfg.com>
2022-03-08 17:25:43 +0700

    use retagging image action

--- retagging-image/entrypoint.sh
+++ retagging-image/entrypoint.sh
@@ -6,6 +6,7 @@ REPOSITORY=$1
 CURRENT=$2
 TAG=$3
 
+yum install -y jq
 MANIFEST=$(aws ecr batch-get-image --repository-name $REPOSITORY --image-ids imageTag=$CURRENT --output json | jq --raw-output '.images[0].imageManifest')
 aws ecr batch-delete-image --repository-name $REPOSITORY --image-ids imageTag=$TAG | jq -r .
 aws ecr put-image --repository-name $REPOSITORY --image-tag $TAG --image-manifest "$MANIFEST" | jq -r .
