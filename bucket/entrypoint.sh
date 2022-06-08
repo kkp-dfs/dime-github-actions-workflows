8767fab0194427bc3cff72791cc5fc3b2e65d0f4
parse-kkp <93644880+parse-kkp@users.noreply.github.com>
2022-06-08 13:59:07 +0700

    Add integration bucket (#79)
    * create folder in testing bucket
    
    * fix invoke bucket action
    
    * move bucket setup action to pipeline
    
    * change folder in testing bucket
    
    * list in s3 bucket
    
    * copy service account to docker action
    
    * get caller identity
    
    * assume role in bucket action

--- bucket/entrypoint.sh
+++ bucket/entrypoint.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
+ 
+yum install -y jq
+TOKEN=$(aws sts assume-role --role-arn $1 --role-session-name $3)
+export AWS_ACCESS_KEY_ID="$(echo $TOKEN | jq '.Credentials.AccessKeyId')"
+export AWS_SECRET_ACCESS_KEY="$(echo $TOKEN | jq '.Credentials.SecretAccessKey')"
+export AWS_SESSION_TOKEN="$(echo $TOKEN | jq '.Credentials.SessionToken')"
+aws sts get-caller-identity
 
-if [[ "$3" == "delete" ]]; then
-    aws s3 rm s3://$1/ --recursive --exclude "*" --include "$2/*"
+if [[ "$4" == "delete" ]]; then
+    aws s3 rm s3://$2/ --recursive --exclude "*" --include "$3/*"
 else
-    aws sts get-caller-identity
-    aws s3api put-object --bucket $1 --key $2
+    aws s3api put-object --bucket $2 --key $3
 fi
 
-echo "::set-output name=bucket::$1"
-echo "::set-output name=folder::$2"
+echo "::set-output name=bucket::$2"
+echo "::set-output name=folder::$3"
