92a27b83895210c9df0b6ed13019906e6db6bdfc
parse-kkp <93644880+parse-kkp@users.noreply.github.com>
2022-06-08 14:06:13 +0700

    Add integration bucket (#80)
    * create folder in testing bucket
    
    * fix invoke bucket action
    
    * move bucket setup action to pipeline
    
    * change folder in testing bucket
    
    * list in s3 bucket
    
    * copy service account to docker action
    
    * get caller identity
    
    * assume role in bucket action
    
    * check getting access key id

--- bucket/entrypoint.sh
+++ bucket/entrypoint.sh
@@ -2,6 +2,7 @@
  
 yum install -y jq
 TOKEN=$(aws sts assume-role --role-arn $1 --role-session-name $3)
+echo $TOKEN | jq '.Credentials.AccessKeyId'
 export AWS_ACCESS_KEY_ID="$(echo $TOKEN | jq '.Credentials.AccessKeyId')"
 export AWS_SECRET_ACCESS_KEY="$(echo $TOKEN | jq '.Credentials.SecretAccessKey')"
 export AWS_SESSION_TOKEN="$(echo $TOKEN | jq '.Credentials.SessionToken')"
