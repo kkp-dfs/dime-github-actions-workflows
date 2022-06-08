9d644532505f589dbeba1d6d9fd26d7d29108c39
parse-kkp <93644880+parse-kkp@users.noreply.github.com>
2022-06-08 14:14:12 +0700

    Add integration bucket (#81)
    * create folder in testing bucket
    
    * fix invoke bucket action
    
    * move bucket setup action to pipeline
    
    * change folder in testing bucket
    
    * list in s3 bucket
    
    * copy service account to docker action
    
    * get caller identity
    
    * assume role in bucket action
    
    * check getting access key id
    
    * fix assigning the jq outputs to variables

--- bucket/entrypoint.sh
+++ bucket/entrypoint.sh
@@ -2,10 +2,9 @@
  
 yum install -y jq
 TOKEN=$(aws sts assume-role --role-arn $1 --role-session-name $3)
-echo $TOKEN | jq '.Credentials.AccessKeyId'
-export AWS_ACCESS_KEY_ID="$(echo $TOKEN | jq '.Credentials.AccessKeyId')"
-export AWS_SECRET_ACCESS_KEY="$(echo $TOKEN | jq '.Credentials.SecretAccessKey')"
-export AWS_SESSION_TOKEN="$(echo $TOKEN | jq '.Credentials.SessionToken')"
+export AWS_ACCESS_KEY_ID="$(echo $TOKEN | jq -r '.Credentials.AccessKeyId')"
+export AWS_SECRET_ACCESS_KEY="$(echo $TOKEN | jq -r '.Credentials.SecretAccessKey')"
+export AWS_SESSION_TOKEN="$(echo $TOKEN | jq -r '.Credentials.SessionToken')"
 aws sts get-caller-identity
 
 if [[ "$4" == "delete" ]]; then
