13eb1181fe479534ba28341b770a83a3d2f9c4ed
treewit-kkp <treewit.chee@dime.co.th>
2022-07-05 11:29:18 +0700

    Exit with error when base image tag is not found

--- retagging-image/entrypoint.sh
+++ retagging-image/entrypoint.sh
@@ -8,5 +8,6 @@ TAG=$3
 
 yum install -y jq
 MANIFEST=$(aws ecr batch-get-image --repository-name $REPOSITORY --image-ids imageTag=$COMMIT --output json | jq --raw-output '.images[0].imageManifest')
+if [ "$MANIFEST" = "null" ]; then echo "ERROR: Image tag ${COMMIT} not found" && exit 1; fi
 aws ecr batch-delete-image --repository-name $REPOSITORY --image-ids imageTag=$TAG | jq -r .
 aws ecr put-image --repository-name $REPOSITORY --image-tag $TAG --image-manifest "$MANIFEST" | jq -r .
